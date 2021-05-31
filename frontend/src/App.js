e14e7f7a26213df1a0f1f43d135acc9da160cebb
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2021-05-31 01:50:14 -0500

    Add react router navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,10 @@
 import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import './App.css';
 import MessageList from './components/MessageList';
 import SignIn from './components/SignIn';
@@ -9,13 +15,32 @@ const App = () => {
   const [token, setToken] = useState(null)
 
   return (
-    <div className="App">
+    <Router>
       <h1>{user ? user.username : ''}</h1>
       <h2>{token}</h2>
-      <MessageList user={user} />
-      <SignIn setUser={setUser} setToken={setToken} />
-      <MessageForm user={user} />
-    </div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/signin'>Sign in</Link>
+        </li>
+        <li>
+          <Link to='/message'>New Message</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path='/signin'>
+          <SignIn setUser={setUser} setToken={setToken} />
+        </Route>
+        <Route path='/message'>
+          <MessageForm user={user} />
+        </Route>
+        <Route path='/'>
+          <MessageList user={user} />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
