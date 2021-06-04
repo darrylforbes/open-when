acb5e0cc92b49a5bc8ee083a155cecb5e056236a
Darryl Forbes <darrylforbes2022@u.northwestern.edu>
2021-06-04 03:25:32 -0500

    Add basic styling to header and individual message

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,33 @@
 import {
   AppBar,
+  makeStyles,
   Toolbar,
   Typography
 } from '@material-ui/core';
 
+const useStyles = makeStyles({
+  toolbar: {
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
+});
+
 const Header = ({ user }) => {
+  const classes = useStyles();
+
   return (
     <AppBar position='static'>
-      <Toolbar>
-        <Typography variant='h1'>Open When</Typography>
-        <Typography variant='h2'>{user ? user.username : null}</Typography>
+      <Toolbar className={classes.toolbar}>
+        <Typography
+          variant='h1'
+        >
+          Open When
+        </Typography>
+        <Typography
+          variant='h2'
+        >
+          {user ? user.username : null}
+        </Typography>
       </Toolbar>
     </AppBar>
   )
--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,26 +1,38 @@
 import {
   Box,
   Button,
+  Backdrop,
+  makeStyles,
   Paper,
   Typography
 } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
+const useStyles = makeStyles({
+  paper: {
+    padding: 50
+  }
+});
+
 const Message = ({ message }) => {
+  const classes = useStyles();
+
   return (
     <Box>
-      <Paper>
-        <Typography variant='h1'>{message.title}</Typography>
-        <Typography variant='h2'>From: {message.sender_id}</Typography>
-        <Typography variant='body1'>{message.body}</Typography>
-        <Button
-          variant='contained'
-          component={ Link }
-          to='/'
-        >
-          Close
-        </Button>
-      </Paper>
+      <Backdrop open={true}>
+        <Paper className={classes.paper}>
+          <Typography variant='h1'>{message.title}</Typography>
+          <Typography variant='h2'>From: {message.sender_id}</Typography>
+          <Typography variant='body1'>{message.body}</Typography>
+          <Button
+            variant='contained'
+            component={ Link }
+            to='/'
+          >
+            Close
+          </Button>
+        </Paper>
+      </Backdrop>
     </Box>
   )
 }
